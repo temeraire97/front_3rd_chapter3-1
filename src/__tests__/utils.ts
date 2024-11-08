5e0aaaf33d52a44eb2189fc3bbe3d1b6de3aabce
temeraire97 <hyensooyoon@gmail.com>
2024-11-08 11:50:14 +0900

    update: useCalendarView test 완료

--- src/__tests__/utils.ts
+++ src/__tests__/utils.ts
@@ -1,3 +1,6 @@
+import { act, renderHook } from '@testing-library/react';
+import { FC } from 'react';
+
 import { fillZero } from '../utils/dateUtils';
 
 export const assertDate = (date1: Date, date2: Date) => {
@@ -10,4 +13,23 @@ export const parseHM = (timestamp: number) => {
   const h = fillZero(date.getHours());
   const m = fillZero(date.getMinutes());
   return `${h}:${m}`;
+};
+
+type SetupHookOptions<T> = {
+  hook: () => T;
+  // eslint-disable-next-line no-unused-vars
+  initialAction?: (result: T) => void | Promise<void>;
+};
+
+// 훅 초기화
+export const setupHook = <T>({ hook, initialAction }: SetupHookOptions<T>) => {
+  const { result } = renderHook(() => hook());
+
+  if (initialAction) {
+    act(() => {
+      initialAction(result.current);
+    });
+  }
+
+  return result;
 };
